f876dfa715ff9e0c5d35ad3deba83416564db22b
robcisneros <robertocisneroslp@gmail.com>
2022-04-04 23:07:03 -0500

    navigation with home n contact

---
+++ components/layout/MainNavigation.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+import classes from './MainNavigation.module.css';
+
+function MainNavigation() {
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>My Portfolio</div>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/'>All projects</Link>
+          </li>
+          <li>
+            <Link href='/contact'>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
---
+++ pages/contact/index.js
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+
+function ContactPage() {
+    return (
+        <Fragment>
+            <h2>Contact page!!</h2>
+        </Fragment>
+    );
+}
+export default ContactPage;
