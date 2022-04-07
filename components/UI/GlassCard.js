0d615b0f46c829d48fadf80e21342a6085736e7e
robcisneros <robertocisneroslp@gmail.com>
2022-04-07 00:49:38 -0500

    adding bgi to project

--- components/UI/GlassCard.js
+++ components/UI/GlassCard.js
@@ -1,14 +1,27 @@
 import classes from "./GlassCard.module.css";
-import Link from 'next/link';
+import Link from "next/link";
+import Image from "next/image";
+import minions from "../../assets/minions.gif";
 
 const GlassCard = (props) => {
   return (
     <Link href={props.href} passHref>
-    <a target="_blank" className={classes.card} >
-      {props.children}
-      <h2>{props.title}</h2>
-      <p> {props.description} </p>
-    </a>
+      <a target="_blank" className={classes.card}>
+        {props.children}
+        
+        <h2>{props.title}</h2>
+        <p> {props.description} </p>
+
+        {/* <Image
+          className={classes.landingImage}
+          src={minions}
+          alt="Minions parade"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        />
+         */}
+      </a>
     </Link>
   );
 };
