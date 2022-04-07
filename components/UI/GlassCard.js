982eba8c5be60f0756618dfc36949cbdd64cd0b0
robcisneros <robertocisneroslp@gmail.com>
2022-04-07 01:10:59 -0500

    fixing header

--- components/UI/GlassCard.js
+++ components/UI/GlassCard.js
@@ -1,7 +1,5 @@
 import classes from "./GlassCard.module.css";
 import Link from "next/link";
-import Image from "next/image";
-import minions from "../../assets/minions.gif";
 
 const GlassCard = (props) => {
   return (
@@ -11,16 +9,7 @@ const GlassCard = (props) => {
         
         <h2>{props.title}</h2>
         <p> {props.description} </p>
-
-        {/* <Image
-          className={classes.landingImage}
-          src={minions}
-          alt="Minions parade"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center"
-        />
-         */}
+        
       </a>
     </Link>
   );
